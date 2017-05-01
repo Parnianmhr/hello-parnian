4fff7337fbc93633d71a207c6eac40695c75b254
Parnian <parnian@parnians-mbp.home>
2017-05-01 19:19:54 +0200

    buttons + -  added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,17 +2,33 @@ import React, { PureComponent } from 'react'
 import './Profile.css'
 
 class Profile extends PureComponent {
-  render(){
+  constructor(props) {
+    super(props)
+    this.state = {
+      height: 100
+    }
+  }
+  render() {
     var { name, age, bio, pic } = this.props;
-    console.log(this.props.name)
+    var { height } = this.state;
     return(
       <div className="profile-box">
         <h2>{name}</h2>
         <h4>age: {age}</h4>
         <h4>bio: {bio}</h4>
-        <img src={pic} alt="selfie" height={100}/>
+        <img src={pic} alt="selfie" height={height}/>
+        <br />
+        <button onClick={this.zoomPicOut.bind(this)}>-</button>
+        <button onClick={this.zoomPicIn.bind(this)}>+</button>
       </div>
     )
+  }
+
+  zoomPicIn() {
+    this.setState({ height: this.state.height + 40})
+  }
+  zoomPicOut() {
+    this.setState({ height: this.state.height - 40})
   }
 }
 
