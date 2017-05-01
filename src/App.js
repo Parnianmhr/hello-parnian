f0dc9a3f14df37f0cea5fe1d8c632eb14102f3c7
Parnian <parnian@parnians-mbp.home>
2017-05-01 20:51:19 +0200

    css fixed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           More will come soon.
         </p>
-        <h1>Parnian is here!</h1>
+        <h1>Profiles</h1>
         <Profile name="Parnian" bio="I love cats" age={38} pic="http://www.clipartbest.com/cliparts/bTy/og6/bTyog6zbc.png"/>
         <Profile name="Frits" bio="I am a cat" age={7} pic="https://s-media-cache-ak0.pinimg.com/236x/cb/d7/7a/cbd77a4db34ac2b8c2a25a395153162f.jpg"/>
       </div>
